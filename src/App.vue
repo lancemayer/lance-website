0c9de50fa56eef065a0a98cc3c88c96b7d335e6f
Lance Klinger <lance@MacBook-Pro.local>
2020-09-08 00:19:10 -0400

    Add Tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="bg-gray-800 h-screen">
+    <div class="bg-gray-900 text-center md:text-left p-5">
+      <router-link
+        class="bg-gray-700 hover:bg-gray-600 text-gray-100 font-bold p-3 rounded-lg mr-3"
+        to="/"
+        >Home</router-link
+      >
+      <router-link
+        class="bg-gray-700 hover:bg-gray-600 text-gray-100 font-bold p-3 rounded-lg"
+        to="/about"
+        >About</router-link
+      >
     </div>
     <router-view />
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
